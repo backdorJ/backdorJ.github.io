84d5320869784ed1934124b5b287235370a519b6
Damir Nabiullin <cdbacjdorz@yandex.ru>
2024-05-28 23:21:38 +0300

    Added carousel

--- index.js
+++ index.js
@@ -17,3 +17,27 @@ btn.addEventListener('click', () => {
     }
 
 });
+
+let currentIndex = 0;
+const carouselItems = document.querySelectorAll('.carousel-item');
+ 
+function goToSlide(index) {
+  if (index < 0) {
+    index = carouselItems.length - 1;
+  } else if (index > carouselItems.length) {
+    index = 0;
+    goToPrevSlide(currentIndex - 1)
+  }
+  currentIndex = index;
+  document.querySelector('.carousel-inner').style.transform = `translateX(-${currentIndex * 30}%)`;
+}
+ 
+function goToNextSlide() {
+  goToSlide(currentIndex + 1);
+}
+ 
+function goToPrevSlide() {
+  goToSlide(currentIndex - 1);
+}
+ 
+setInterval(goToNextSlide, 3000);
